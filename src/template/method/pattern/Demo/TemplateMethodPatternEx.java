f253a871bceaa259ca849e8d0eb09d097aa721f5
sobamemil <sieh96@naver.com>
2021-03-18 14:38:00 +0900

    template method pattern

---
+++ src/template/method/pattern/Demo/TemplateMethodPatternEx.java
@@ -0,0 +1,16 @@
+package template.method.pattern.Demo;
+import engineering.papers.*;
+
+public class TemplateMethodPatternEx {
+    public static void main(String[] args) {
+        System.out.println("***Template Method Pattern Demo***\n");
+
+        BasicEngineering bs = new ComputerScience();
+        System.out.println("Computer Sc Papers:");
+        bs.Papers();
+        System.out.println();
+        bs = new Electronics();
+        System.out.println("Electronics Papers:");
+        bs.Papers();
+    }
+}
