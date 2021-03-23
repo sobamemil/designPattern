b4c97c542e9a563973c6c098a777cd11a8bb6127
sobamemil <sieh96@naver.com>
2021-03-23 20:44:35 +0900

    Prototype Pattern

---
+++ src/car/Ford.java
@@ -0,0 +1,12 @@
+package car;
+
+public class Ford extends BasicCar {
+    public Ford(String m) {
+        modelname = m;
+    }
+
+    @Override
+    public BasicCar clone() throws CloneNotSupportedException {
+        return (Ford)super.clone();
+    }
+}
