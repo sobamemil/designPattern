940a6e0e4054fb61ee8092b9b2061e0774735e8e
sobamemil <56301498+sobamemil@users.noreply.github.com>
2021-05-12 23:59:53 +0900

    Add files via upload
    mewdiator pattern

---
+++ mediator/src/ColleagueButton.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+
+public class ColleagueButton extends Button implements Colleague {
+    private Mediator mediator;
+    public ColleagueButton(String caption) {
+        super(caption);
+    }
+
+    public void setMediator(Mediator mediator) { // Mediator를 저장
+        this.mediator = mediator;
+    }
+
+    public void setColleagueEnabled(boolean enabled) { // Mediator에서 유효/무효를 지시
+        setEnabled(enabled);
+    }
+}
